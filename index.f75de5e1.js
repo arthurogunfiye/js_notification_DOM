0121c413afbf6823c427e01762522d2dd417efcf
Arthur Ogunfuye <arthurity@yahoo.co.uk>
2025-04-29 07:39:32 +0100

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,27 @@
+"use strict";
+const body = document.querySelector("body");
+const pushNotification = (posTop, posRight, title, description, type)=>{
+    const notificationCard = document.createElement("div");
+    notificationCard.classList.add("notification");
+    notificationCard.style.top = `${posTop}px`;
+    notificationCard.style.right = `${posRight}px`;
+    const notificationTitle = document.createElement("h2");
+    notificationTitle.classList.add("title");
+    notificationTitle.textContent = title;
+    const notificationInfo = document.createElement("p");
+    notificationInfo.innerText = description;
+    notificationCard.appendChild(notificationTitle);
+    notificationCard.appendChild(notificationInfo);
+    body.appendChild(notificationCard);
+    if (type === "success") notificationCard.classList.add("success");
+    else if (type === "error") notificationCard.classList.add("error");
+    else if (type === "warning") notificationCard.classList.add("warning");
+    setTimeout(()=>{
+        notificationCard.style.display = "none";
+    }, 2000);
+};
+pushNotification(10, 10, "Title of Success message", "Message example.\n Notification should contain title and description.", "success");
+pushNotification(150, 10, "Title of Error message", "Message example.\n Notification should contain title and description.", "error");
+pushNotification(290, 10, "Title of Warning message", "Message example.\n Notification should contain title and description.", "warning");
+
+//# sourceMappingURL=index.f75de5e1.js.map
